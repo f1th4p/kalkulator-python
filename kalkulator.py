bf28a340566c715f79af0b3dc23d8a353d1c2196
f1th4p <f1th4p@gmail.com>
2025-02-15 13:53:34 +0100

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from scipy.optimize import brentq
 
 def calculate_net_old(P, C, ZUS, target_net=6350.00, tax_rate=0.12, tax_deduction=300, health_rate=0.09):
@@ -19,22 +20,20 @@ def calculate_net_old(P, C, ZUS, target_net=6350.00, tax_rate=0.12, tax_deductio
     tax = tax_rate * base - tax_deduction
     health = health_rate * base
     net = base - tax - health
-    # print(f"Debug: P={P:.2f}, C={C:.2f}, ZUS={ZUS:.2f}, base={base:.2f}, tax={tax:.2f}, health={health:.2f}, net={net:.2f}")
     return net - target_net
 
-# Przykładowe parametry z zeszłego roku:
+# Przykładowe parametry
 C_example = 252.15    # Koszty
 ZUS_example = 1485.31 # Społeczne (składki społeczne)
 target_net = 6350.00  # Docelowa kwota "na rękę"
 
-# Zakres poszukiwań przychodu – dobierzemy taki, aby zawierał wartość z przykładu (9285 zł)
+# Zakres poszukiwań przychodu
 P_min, P_max = 9000, 10000
 
-# P_solution = brentq(calculate_net_old, P_min, P_max, args=(C_example, ZUS_example, target_net))
-# print(f"Rozwiązanie: Przychód = {P_solution:.2f} zł")
+# Obliczenia
+P_solution_1 = brentq(calculate_net_old, P_min, P_max, args=(263.22, 1646.47, target_net))
+P_solution_2 = brentq(calculate_net_old, P_min, P_max, args=(263.22, 1773.96, target_net))
 
-P_solution = brentq(calculate_net_old, P_min, P_max, args=(263.22, 1646.47, target_net))
-print(f"Rozwiązanie: Przychód nowy ZUS bez chorobowego = {P_solution:.2f} zł")
-
-P_solution = brentq(calculate_net_old, P_min, P_max, args=(263.22, 1773.96, target_net))
-print(f"Rozwiązanie: Przychód nowy ZUS z chorobowym = {P_solution:.2f} zł")
+# Wyświetlanie wyników w aplikacji Streamlit
+st.write(f"Rozwiązanie: Przychód nowy ZUS bez chorobowego = {P_solution_1:.2f} zł")
+st.write(f"Rozwiązanie: Przychód nowy ZUS z chorobowym = {P_solution_2:.2f} zł")
